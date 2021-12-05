d97b6fe1a132dd06375e32cb3ffc626a727b974d
joseviedmacorreo <68796537+joseviedmacorreo@users.noreply.github.com>
2021-12-05 09:53:24 -0300

    ver
    ver

---
+++ Tema 1/01_test.R
@@ -0,0 +1,5 @@
+#Aca puedo comentar, con ALT + ENTER ejecuta abajo en consola
+#tb tiene un run para la linea codigo actual y el de al lado para todo
+
+2 + 3
+
---
+++ Tema 1/03_install_libraries.R
@@ -0,0 +1,12 @@
+#install.packages("tidyverse", dep = TRUE)
+#ver que esto instala el paquete, pero no lo carga, para cargarlo uso:
+
+#library(tidyverse)
+
+
+install.packages("magic", dep = TRUE)
+library(magic)
+magic(6)
+
+installed.packages()
+#Muestra los paquetes que tengo instalado
